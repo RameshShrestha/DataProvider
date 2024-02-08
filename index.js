ba27c699637ee16fded6d76e41a18a3882e62246
Ramesh Shrestha <fx_ra@hotmail.com>
2024-02-08 13:13:05 +0530

    Update index.js
    Update

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(express.json());
 app.use(express.static("express"));
 app.use(apiRequestLimiter);
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https:rameshdataprovider.onrender.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   next();
